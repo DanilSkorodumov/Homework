bdb5619f562f8e3d9c78b6de37ee1e6ba0a39b6e
DanilSkorodumov <skorodumovdanil40@gmail.com>
2024-09-07 20:55:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# "1st program".
+print(9**0.5*5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+print(9.99 > 9.98 or 1000 != 1000.1)
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == 2*(2+2))
+# 4th program
+print((int(float('123.456')* 10))%10)
