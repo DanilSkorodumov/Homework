2183c57fb3826d5423a3cf78f9fbd5da35247544
Danil Skorodumov <skorodumovdanil40@gmail.com>
2024-10-01 14:29:22 +0500

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,6 @@
 def get_multiplied_digits (number):
     str_number = str(number)
+    str_number = str_number.replace('0','')
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
