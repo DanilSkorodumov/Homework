fb2083a7fbbddbce80d2a6f79569d40ce13370ae
Danil Skorodumov <skorodumovdanil40@gmail.com>
2024-10-05 20:11:04 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as div1
+from true_math import divide as div2
+
+result1 = div1(69, 3)
+result2 = div1(3, 0)
+result3 = div2(49, 7)
+result4 = div2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
